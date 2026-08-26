e5ba583b4f01d7afc94a72579302b794091d64fe
danivit2003-a11y <dani.vit2003@gmail.com>
2026-08-26 16:14:18 +0100

    ksh<gfklh

--- supabase-config.js
+++ supabase-config.js
@@ -4,5 +4,12 @@ const SUPABASE_ANON_KEY = "sb_publishable_IzimpwyAxiz8VwxsKy8UIw_ldefjU6a";
 
 const uneraSupabase = window.supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_ANON_KEY
+    SUPABASE_ANON_KEY,
+    {
+        auth: {
+            persistSession: true,
+            autoRefreshToken: true,
+            detectSessionInUrl: true
+        }
+    }
 );
